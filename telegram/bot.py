51a17f318996ea4c038cb1336acea89661bc6376
sithum-code <92231678+sithum-code@users.noreply.github.com>
2021-10-10 06:28:40 +0530

    Update bot.py

--- telegram/bot.py
+++ telegram/bot.py
@@ -39,11 +39,10 @@ knownUsers = {}
 #
 #
 #Pass all message(exclude /start, /on, /help), if user not activate:
-@bot.message_handler(func=lambda message: 
-                    ((knownUsers.get(message.chat.id) == None) or (knownUsers.get(message.chat.id) == 1)) 
-                    markup_inline = types.InlineKeyboardMarkup()
-    btn1 = InlineKeyboardButton(text = 'Да', callback_data = 'Yes')
-    btn2 = InlineKeyboardButton(text = 'Нет', callback_data = 'No')
+@bot.message_handler(func=lambda message: \
+                    ((knownUsers.get(message.chat.id) == None) or (knownUsers.get(message.chat.id) == 1)) \
+                    and (message.text != '/start') and (message.text != '/on') \
+                    and (message.text != '/help'), content_types=["text"])
 def pass_message(message):
     pass
 
